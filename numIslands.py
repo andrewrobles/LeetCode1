42ddf7aa6769ed8524c3f79980ed95aea85cd0be
Andrew Robles <me@andrewrobles.com>
2021-04-25 17:22:40 -0700

    Change islands to numbers instead of strings

--- numIslands.py
+++ numIslands.py
@@ -5,7 +5,7 @@ def countIslands(self, grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             
-            if grid[i][j] == '1':
+            if grid[i][j] == 1:
                 count += 1
                 
                 bfs(grid, i, j)
@@ -13,10 +13,10 @@ def countIslands(self, grid):
     return count
     
 def bfs(grid, i, j):
-    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == 0:
         return
 
-    grid[i][j] = '0'
+    grid[i][j] = 0
 
     bfs(grid, i+1, j)
     bfs(grid, i-1, j)
