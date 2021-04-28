af456a992a15aeda2c09495d849cb422d21c654e
Andrew Robles <me@andrewrobles.com>
2021-04-28 05:41:07 -0700

    breadthFirstSearch

---
+++ breadthFirstSearch.py
@@ -0,0 +1,28 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+def breadthFirstSearch(graph, node):
+    visited = []
+    queue = []
+
+    visited.append(node)
+    queue.append(node)
+    
+    while queue:
+        curr = queue.pop(0)
+        print(curr)
+
+        for child in graph[curr]:
+
+            if child not in visited:
+                visited.append(child)
+                queue.append(child) 
+
+# Driver code
+breadthFirstSearch(graph, 'A')
