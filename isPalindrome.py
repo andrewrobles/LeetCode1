266f696b31659db18240e511eb6644a50ff0dc74
Andrew Robles <me@andrewrobles.com>
2021-05-04 06:10:30 -0700

    isPalindrome

---
+++ isPalindrome.py
@@ -0,0 +1,7 @@
+def isPalindrome(head):
+    vals = []
+    currentNode = head
+    while currentNode is not None:
+        vals.append(currentNode.val)
+        currentNode = currentNode.next
+    return vals == vals[::-1]
