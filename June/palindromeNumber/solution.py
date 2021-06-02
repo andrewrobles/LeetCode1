c16239318c03946f428985c5c8a7e28de12e321b
andrewrobles <andrewroblesdev@gmail.com>
2021-06-02 13:00:55 -0700

    Palindrome number

--- June/palindromeNumber/solution.py
+++ June/palindromeNumber/solution.py
@@ -1,3 +1,17 @@
 def palindromeNumber(x):
     '''Reverse the number and compare it original'''
-    pass
+
+    xRef, xFlipped = x, 0
+
+    while xRef > 0:
+        # Left shift flipped
+        xFlipped *= 10
+
+        # Add ones place to flipped
+        xFlipped += xRef % 10
+
+        # Right shift reference 
+        xRef //= 10
+
+    # Compare flipped to original
+    return x == xFlipped
