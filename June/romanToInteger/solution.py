1cdb01e2207ecbecac5da5287a8bb10cf40fdce3
andrewrobles <andrewroblesdev@gmail.com>
2021-06-03 19:42:45 -0700

    Remove comments

--- June/romanToInteger/solution.py
+++ June/romanToInteger/solution.py
@@ -1,11 +1,4 @@
 def romanToInteger(s):
-    '''
-    Read from left to right and add numbers as necessary.
-
-    If special case is found then subtract that number
-    ''' 
-    value = 0 
-
     symbols = {
         'I': 1,
         'V': 5,
@@ -16,20 +9,17 @@ def romanToInteger(s):
         'M': 1000,
     }
 
+    value = 0
+
     for i in range(len(s)):
 
         currDigit = s[i]
-
         sumVal = symbols[currDigit]
 
-        # Out of bounds check
         if i < len(s) - 1:
             nextDigit = s[i + 1]
-
-            # Subtract sum value if special case
             if symbols[currDigit] < symbols[nextDigit]:
                 sumVal *= -1
 
         value += sumVal
-
     return value
