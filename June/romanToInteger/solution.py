771fcf370467012b57e0b16977bec8c955724654
Andrew Robles <andrewroblesdev@gmail.com>
2021-06-03 18:52:29 -0700

    Refactor solution

--- June/romanToInteger/solution.py
+++ June/romanToInteger/solution.py
@@ -22,30 +22,14 @@ def romanToInteger(s):
 
         sumVal = symbols[currDigit]
 
-        if isSpecialCase(s, i):
-            sumVal *= -1
-            
+        # Out of bounds check
+        if i < len(s) - 1:
+            nextDigit = s[i + 1]
+
+            # Subtract sum value if special case
+            if symbols[currDigit] < symbols[nextDigit]:
+                sumVal *= -1
+
         value += sumVal
 
     return value
-
-def isSpecialCase(s, i):
-    currDigit = s[i]
-
-    # Out of bounds check
-    if i < len(s) - 1:
-        nextDigit = s[i + 1]
-
-        # Check for special case: I
-        if currDigit == 'I' and (nextDigit == 'V' or nextDigit == 'X'):
-            return True
-        
-        # Check for special case: X
-        if currDigit == 'X' and (nextDigit == 'L' or nextDigit=='C'):
-            return True
-        
-        # Check for special case: C
-        if currDigit == 'C' and (nextDigit == 'D' or nextDigit=='M'):
-            return True 
-
-    return False 
